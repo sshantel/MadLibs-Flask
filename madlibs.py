9481b0659ebd1e266c44b8384b04512d4a455e7e
Chantel Yip <chantelyip422@gmail.com>
2020-05-13 23:59:11 -0700

    adding game.html and goodbye.html

--- madlibs.py
+++ madlibs.py
@@ -11,35 +11,46 @@ app = Flask(__name__)
 AWESOMENESS = [
     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
-    'smashing', 'lovely',
-]
-
+    'smashing', 'lovely']
 
 @app.route('/')
 def start_here():
     """Display homepage."""
-
     return "Hi! This is the home page."
-
 
 @app.route('/hello')
 def say_hello():
     """Say hello to user."""
-
-    return render_template("hello.html")
+    return render_template('hello.html')
 
 
 @app.route('/greet')
 def greet_person():
     """Greet user with compliment."""
 
-    player = request.args.get("person")
+    player = request.args.get('person')
 
     compliment = choice(AWESOMENESS)
 
-    return render_template("compliment.html",
-                           person=player,
-                           compliment=compliment)
+    return render_template('compliment.html', 
+                            person = player,
+                            compliment = compliment)
+
+@app.route('/game')
+def show_madlib_form():
+
+    play_game = request.args.get("user_choice")
+
+    if play_game == 'yes':
+        return render_template('game.html')
+    else:
+        return render_template('goodbye.html')
+ 
+
+
+ 
+
+    
 
 
 if __name__ == '__main__':
